a751b2c1cad0bb0e64eab97e9e16adb0c5cd4d19
alex <alexhao2017@gmail.com>
2019-01-29 17:19:16 +1100

    K-classfier

--- kNN.py
+++ kNN.py
@@ -20,3 +20,45 @@ def classify0(inX,dataSet,labels,k):
     return sortedClassCount[0][0]
 
 
+def file2matirx(filename):
+    fr=open(filename)
+    arrayOLines=fr.readlines()
+    numberOfLines=len(arrayOLines)
+    returnMat=zeros((numberOfLines,3))
+
+    classLabelVector=[]
+    index=0
+    for line in arrayOLines:
+        line=line.strip()
+        listFromLine=line.split('\t')
+        returnMat[index,:]=listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index+=1
+    return returnMat,classLabelVector
+def autonorm(dataSet):
+    minvals=dataSet.min(0)
+    maxvals=dataSet.max(0)
+    ranges=maxvals-minvals
+    normDataSet=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSet=dataSet-tile(minvals,(m,1))
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minvals
+
+def datingClassTest():
+    hoRatio=0.1
+
+    datingDataMat,datingLabels=file2matirx('datingTestSet2.txt')
+    normMat,ranges,minVals=autonorm(datingDataMat)
+    m=normMat.shape[0]
+    numTestVecs=int(m*hoRatio)
+    errorCount=0.0
+    for i in range(numTestVecs):
+        classifierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print('the classifier came back with :%d,the real answer is :%d' %(classifierResult,datingLabels[i]))
+        if classifierResult!=datingLabels[i]:
+            errorCount+=1
+    print(numTestVecs)
+    print('error rate is :%f'%(errorCount/float(numTestVecs)))
+
+datingClassTest()
