c509e786046c5f54f693a96e04db853080d7d9ca
alex <alexhao2017@gmail.com>
2019-01-30 14:55:10 +1100

    K-classfier

--- kNN.py
+++ kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+from os import listdir
 
 def createDataSet():
     group=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -61,4 +62,39 @@ def datingClassTest():
     print(numTestVecs)
     print('error rate is :%f'%(errorCount/float(numTestVecs)))
 
-datingClassTest()
+def img2vector(filename):
+    returnvect=zeros((1,1024))
+    fr=open(filename)
+    for i in range(32):
+        lineStr=fr.readline()
+        for j in range(32):
+            returnvect[0,32*i+j]=int(lineStr[j])
+    return returnvect
+def handWritingClassTest():
+    hwLabels=[]
+    trainingFileList=listdir('trainingDigits')
+    m=len(trainingFileList)
+    trainingMat=zeros((m,1024))
+    for i in range(m):
+        fileNmaeStr=trainingFileList[i]
+        fileStr=fileNmaeStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:]=img2vector('trainingDigits/%s' % fileNmaeStr)
+    testFileList=listdir('testDigits')
+
+    errorCount=0
+    mTest=len(testFileList)
+    for i in range(mTest):
+        fileNameStr=testFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        print(fileStr,fileNameStr.split('.'))
+        classNumStr=int(fileStr.split('_')[0])
+        vectorUnderTest=img2vector('testDigits/%s' % fileNameStr)
+        classifierResult=classify0(vectorUnderTest,trainingMat,hwLabels,3)
+
+        print('the classifier came back with %d,the real anser is %d' % (classifierResult,classNumStr))
+        if classifierResult!=classNumStr:
+            errorCount+=1
+    print('error number is %d' %errorCount)
+    print('error rate %f' % (errorCount/float(mTest)))
